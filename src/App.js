654f91d8bc1446069ab5e7b9b9a1a996cb2cc63e
Long23112002 <115551484+Long23112002@users.noreply.github.com>
2023-10-31 00:25:19 +0700

    ok

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import "./style/style.css";
+import Overlay from "./components/Overlay";
+import FormSignin from "./components/FormSignin";
+import FormSignup from "./components/FormSignup";
+function App() {
+
+  const containerRef = useRef(null);
+
+  const handleSignInClick = () => {
+    if (containerRef.current) {
+      containerRef.current.classList.add('active');
+    }
+  };
+
+  const handleSignUpClick = () => {
+    if (containerRef.current) {
+      containerRef.current.classList.remove('active');
+    }
+  };
+  return (
+    <div className="App">
+      <div className="container" ref={containerRef}>
+        <div className="box">
+          <FormSignin />
+          <FormSignup />
+        </div>
+
+        <Overlay onSignInClick={handleSignInClick}  onSignUpClick={handleSignUpClick}/>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
